c42ddee9f15cc5ff57c27224ca44c3382aa4da41
dswelam <dswelam@ncsu.edu>
2025-11-16 21:35:14 -0500

    [Implementation] Updated UserRepository
    Changed `findByName(String)` to return `Optional<Role>` instead of Role
    to avoid nulls.
    
    Ref #45

--- wolf-cafe-backend/src/main/java/edu/ncsu/csc326/wolfcafe/repository/RoleRepository.java
+++ wolf-cafe-backend/src/main/java/edu/ncsu/csc326/wolfcafe/repository/RoleRepository.java
@@ -1,18 +1,23 @@
 package edu.ncsu.csc326.wolfcafe.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.ncsu.csc326.wolfcafe.entity.Role;
 
 /**
  * Repository interface for Roles.
+ *
+ * @author Dania Swelam
  */
 public interface RoleRepository extends JpaRepository<Role, Long> {
-	
-	/**
-	 * Finds the role by name.
-	 * @param name role's name
-	 * @return Role object
-	 */
-    Role findByName(String name);
+
+    /**
+     * Finds the role by name.
+     *
+     * @param name role's name
+     * @return Role object
+     */
+    Optional<Role> findByName ( String name );
 }
